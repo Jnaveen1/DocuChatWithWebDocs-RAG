785893b9f461d7cc4f4eee925461916192a7269f
Jnaveen1 <naveenjanapati65@gmail.com>
2026-04-15 15:11:46 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,98 @@
+from dotenv import load_dotenv
+import os
+
+from langchain.chat_models import init_chat_model 
+from langchain_community.document_loaders import WebBaseLoader 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma 
+
+load_dotenv()
+
+urls = [
+    "https://docs.langchain.com/oss/python/integrations/document_loaders", 
+    "https://docs.langchain.com/oss/python/integrations/vectorstores"  ,
+    "https://docs.langchain.com/oss/python/integrations/text_embedding" 
+]
+
+loader = WebBaseLoader(urls)
+docs = loader.load() 
+
+split_docs = RecursiveCharacterTextSplitter(
+    chunk_size=1000, 
+    chunk_overlap=500
+)
+
+all_splits = split_docs.split_documents(docs)
+
+print(all_splits)
+print(all_splits[0].metadata)
+print(all_splits[0].page_content)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+)
+
+vector_store = Chroma(
+    collection_name="example_collection", 
+    embedding_function=embeddings, 
+    persist_directory="./chroma_langchain_db"
+)
+
+doc_ids = vector_store.add_documents(documents=all_splits) 
+
+sample = vector_store.get(limit=1, include=["embeddings" , "documents"])
+
+print(sample)
+print(sample[:3])
+
+api_key=os.getenv("GEMINI_API_KEY")
+
+model = init_chat_model(
+    "google_genai:gemini-2.5-flash",
+    api_key=api_key
+)
+
+def retrieve_context(query : str, k: int = 2):
+    retrieved_docs=vector_store.similarity_search(query, k=k)
+    docs_content = ""
+    for doc in retrieved_docs:
+        docs_content += f"Source: {doc.metadata}\n"
+        docs_content += f"Content: {docs.page_content}\n\n"
+    
+    return docs_content , retrieved_docs 
+    
+
+def ask_about_pdf(user_query):
+  context, source_docs = retrieve_context(user_query, k=2)
+  system_message = f"""You are a helpful chatbot.
+                     Use only the following pieces of context to answer the 
+                     question. Don't makeup any new information: {context} """
+
+  messages = [
+    {"role": "system", "content": system_message},
+    {"role": "user", "content": user_query}
+  ]
+
+  response = model.invoke(messages) 
+  return {
+    "answer" : response.content , 
+    "source_documents": source_docs ,  
+    "context_used": context 
+  }
+
+result = ask_about_pdf("How to use the HuggingFaceEmbeddings?")
+print(result)
+print(result["answer"])
+
+result = ask_about_pdf("Explain the use this value : sentence-transformers/all-mpnet-base-v2")
+print(result)
+print(result["answer"])
+
+result = ask_about_pdf("How to use Open AI Embeddings")
+print(result)
+print(result["answer"])
+
+result = ask_about_pdf("Explain about this: OpenAIEmbeddings")
+print(result)
+print(result["answer"])
